0f62df5b33bb521d2e8ae67c299cfe9998c91af0
anthony <nvhoanganh1909@gmail.com>
2020-05-02 23:12:20 +1000

    return JSON

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,23 @@
 import * as functions from 'firebase-functions';
+
+const admin = require('firebase-admin');
+admin.initializeApp();
+const db = admin.firestore();
+
 export const sessions = functions
 	.region('asia-northeast1')
 	.https.onRequest((request, response) => {
-		response.send(['hello world']);
+		console.log(request.query.id);
+		return db
+			.collection('sessions')
+			.doc(request.query.id)
+			.collection('estimatedStories')
+			.get()
+			.then((d: any) => {
+				const docs = d.docs.map((x: any) => ({
+					id: x.id,
+					...x.data(),
+				}));
+				response.send(docs);
+			});
 	});
