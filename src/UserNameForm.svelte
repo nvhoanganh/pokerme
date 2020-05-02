a5578ad3985585729e9be75ebac7ed6790260d66
anthony <nvhoanganh1909@gmail.com>
2020-05-02 15:23:34 +1000

    add max length to 15

--- src/UserNameForm.svelte
+++ src/UserNameForm.svelte
@@ -25,7 +25,7 @@
         </label>
         <input
           minlength="3"
-          maxlength="20"
+          maxlength="15"
           bind:this={usernameInput}
           class="shadow appearance-none border rounded w-full py-2 px-3
           text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
